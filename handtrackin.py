82429c66ad769482e040cfae9be1ea9baaa470cb
medjlaiel <medjlaiel34@gmail.com>
2025-09-30 10:33:53 +0100

    Initial commit - hand Gesture project

---
+++ handtrackin.py
@@ -0,0 +1,69 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import pyautogui
+from math import hypot
+
+# -----------------------------
+# Initialize MediaPipe Hands
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(max_num_hands=1)  # track 1 hand
+mp_draw = mp.solutions.drawing_utils
+
+# Webcam
+cap = cv2.VideoCapture(0)
+
+# Get screen size
+screen_width, screen_height = pyautogui.size()
+
+# Previous cursor point for smoothing
+prev_x, prev_y = 0, 0
+smooth_factor = 5
+
+# -----------------------------
+while True:
+    success, img = cap.read()
+    if not success:
+        continue
+
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img_rgb)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+            # Get finger tips
+            thumb_x = int(hand_landmarks.landmark[4].x * img.shape[1])
+            thumb_y = int(hand_landmarks.landmark[4].y * img.shape[0])
+            index_x = int(hand_landmarks.landmark[8].x * img.shape[1])
+            index_y = int(hand_landmarks.landmark[8].y * img.shape[0])
+
+            # -----------------------------
+            # Volume control (thumb + index pinch)
+            distance = hypot(index_x - thumb_x, index_y - thumb_y)
+            if distance < 40:  # pinch detected
+                pyautogui.press("volumedown")  # adjust volume
+                cv2.putText(img, "Volume Down", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+            elif distance > 150:
+                pyautogui.press("volumeup")
+                cv2.putText(img, "Volume Up", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+
+            # -----------------------------
+            # Cursor movement with index finger
+            cursor_x = np.interp(index_x, [0, img.shape[1]], [0, screen_width])
+            cursor_y = np.interp(index_y, [0, img.shape[0]], [0, screen_height])
+
+            # Smooth movement
+            smooth_x = prev_x + (cursor_x - prev_x) / smooth_factor
+            smooth_y = prev_y + (cursor_y - prev_y) / smooth_factor
+            pyautogui.moveTo(smooth_x, smooth_y)
+            prev_x, prev_y = smooth_x, smooth_y
+
+    # Show webcam feed
+    cv2.imshow("Hand Tracking - Cursor & Volume", img)
+    if cv2.waitKey(1) & 0xFF == 27:  # ESC to exit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
