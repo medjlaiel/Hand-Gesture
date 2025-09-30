a13379c4b8d316fddf48327a3c82b1d871881c6c
medjlaiel <medjlaiel34@gmail.com>
2025-09-30 11:19:18 +0100

    Update hand tracking code with left/right click gestures

--- handtrackin.py
+++ handtrackin.py
@@ -3,6 +3,7 @@ import mediapipe as mp
 import numpy as np
 import pyautogui
 from math import hypot
+import time
 
 # -----------------------------
 # Initialize MediaPipe Hands
@@ -19,6 +20,11 @@ screen_width, screen_height = pyautogui.size()
 # Previous cursor point for smoothing
 prev_x, prev_y = 0, 0
 smooth_factor = 5
+
+# Click cooldown timers
+last_left_click = 0
+last_right_click = 0
+click_cooldown = 0.5  # seconds
 
 # -----------------------------
 while True:
@@ -38,14 +44,18 @@ while True:
             thumb_y = int(hand_landmarks.landmark[4].y * img.shape[0])
             index_x = int(hand_landmarks.landmark[8].x * img.shape[1])
             index_y = int(hand_landmarks.landmark[8].y * img.shape[0])
+            middle_x = int(hand_landmarks.landmark[12].x * img.shape[1])
+            middle_y = int(hand_landmarks.landmark[12].y * img.shape[0])
+            pinky_x = int(hand_landmarks.landmark[20].x * img.shape[1])
+            pinky_y = int(hand_landmarks.landmark[20].y * img.shape[0])
 
             # -----------------------------
             # Volume control (thumb + index pinch)
-            distance = hypot(index_x - thumb_x, index_y - thumb_y)
-            if distance < 40:  # pinch detected
-                pyautogui.press("volumedown")  # adjust volume
+            distance_thumb_index = hypot(index_x - thumb_x, index_y - thumb_y)
+            if distance_thumb_index < 40:  # pinch detected
+                pyautogui.press("volumedown")
                 cv2.putText(img, "Volume Down", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-            elif distance > 150:
+            elif distance_thumb_index > 150:
                 pyautogui.press("volumeup")
                 cv2.putText(img, "Volume Up", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
 
@@ -54,14 +64,28 @@ while True:
             cursor_x = np.interp(index_x, [0, img.shape[1]], [0, screen_width])
             cursor_y = np.interp(index_y, [0, img.shape[0]], [0, screen_height])
 
-            # Smooth movement
             smooth_x = prev_x + (cursor_x - prev_x) / smooth_factor
             smooth_y = prev_y + (cursor_y - prev_y) / smooth_factor
             pyautogui.moveTo(smooth_x, smooth_y)
             prev_x, prev_y = smooth_x, smooth_y
 
+            # -----------------------------
+            # Left Click (index + middle pinch)
+            distance_index_middle = hypot(index_x - middle_x, index_y - middle_y)
+            if distance_index_middle < 30 and time.time() - last_left_click > click_cooldown:
+                pyautogui.click(button='left')
+                last_left_click = time.time()
+                cv2.putText(img, "Left Click", (50,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+
+            # Right Click (thumb + pinky pinch)
+            distance_thumb_pinky = hypot(thumb_x - pinky_x, thumb_y - pinky_y)
+            if distance_thumb_pinky < 30 and time.time() - last_right_click > click_cooldown:
+                pyautogui.click(button='right')
+                last_right_click = time.time()
+                cv2.putText(img, "Right Click", (50,150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+
     # Show webcam feed
-    cv2.imshow("Hand Tracking - Cursor & Volume", img)
+    cv2.imshow("Hand Tracking - Cursor, Volume & Click", img)
     if cv2.waitKey(1) & 0xFF == 27:  # ESC to exit
         break
 
